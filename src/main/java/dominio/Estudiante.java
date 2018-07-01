9d52e049703f5d7e40b131aa701acd4ff6285442
AndresBettinelli <bettinelli.andres@gmail.com>
2018-07-01 19:33:10 -0300

    Mejora clase Estudiante
    Se agrega las variables que piden, constructor y getters

--- src/main/java/dominio/Estudiante.java
+++ src/main/java/dominio/Estudiante.java
@@ -1,7 +1,43 @@
 package dominio;
 
-public class Estudiante {
+import java.util.Collection;
 
-	
-	
+import dominio.Asignacion;
+
+public class Estudiante {
+	private String nombre;
+	private String apellido;
+	private int legajo;
+	private String usuarioGit;
+	private Collection<Asignacion> asignaciones;
+
+	public Estudiante(String nombre, String apellido, int legajo, 
+			String usuarioGit, Collection<Asignacion> asignaciones) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.legajo = legajo;
+		this.usuarioGit = usuarioGit;
+		this.asignaciones = asignaciones;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public int getLegajo() {
+		return legajo;
+	}
+
+	public String getUsuarioGit() {
+		return usuarioGit;
+	}
+
+	public Collection<Asignacion> getAsignaciones() {
+		return asignaciones;
+	}
+
 }
